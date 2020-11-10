b3d91005efcd54c407e96de2e294c10c8fd09230
baseoleph <baseoleph@gmail.com>
2020-11-10 18:18:37 +0300

    02 blinking ready

--- 02_blinking_led/Core/Src/main.c
+++ 02_blinking_led/Core/Src/main.c
@@ -1,19 +1,27 @@
-#include "main.h"
-#include "stm32f1xx_hal.h"
+#include "stdint.h"
 
 int main(void)
 {
+    uint32_t Reset_and_Clock_Control = 0x40021000UL;
+    uint32_t APB2ENR = 0x18UL;
+    uint32_t *RCC_APB2ENR = Reset_and_Clock_Control + APB2ENR;
 
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
-    while(__HAL_RCC_GPIOA_IS_CLK_DISABLED());
+    uint32_t PORTX_ENABLE = 0x1;
+    uint32_t IOPAEN = 0x2;
+    uint32_t PORTA_ENABLE = PORTX_ENABLE << IOPAEN;
+    *RCC_APB2ENR |= PORTA_ENABLE;
 
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = LD2_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    while((*RCC_APB2ENR & PORTA_ENABLE) == 0);
+
+    uint32_t GPIOA__ = 0x40010800;
+    uint32_t BSRR__ = 0x10;
+    uint32_t *GPIOA_BSRR__ = GPIOA__ + BSRR__;
+    uint32_t *GPIOA_CRL__ = GPIOA__ + 0;
+
+    *GPIOA_CRL__ &= ~(0b11 << 22);
+    *GPIOA_CRL__ |= 0b1 << 20;
+
+    *GPIOA_BSRR__ |= 0x20;
   while (1)
   {
   }
