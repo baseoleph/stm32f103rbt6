ee197a3d73989d82506d1f7d08b5e0a81f3d0f7f
baseoleph <baseoleph@gmail.com>
2020-11-10 12:02:04 +0300

    test create proj which works on w10 and linux

---
+++ untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/DependInfo.cmake
@@ -0,0 +1,72 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/baseoleph/CLionProjects/untitled/Core/Startup/startup_stm32f103rbtx.s" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Startup/startup_stm32f103rbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/main.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/main.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/stm32f1xx_hal_msp.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/stm32f1xx_it.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/stm32f1xx_it.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/syscalls.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/syscalls.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/sysmem.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/sysmem.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Core/Src/system_stm32f1xx.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Core/Src/system_stm32f1xx.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/home/baseoleph/CLionProjects/untitled/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "/home/baseoleph/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
