0460d5abc12fefd0c4848ba64d6e2be4858f2a90
baseoleph <baseoleph@gmail.com>
2020-11-10 21:27:59 +0300

    02 hse is work. probably

--- 02_blinking_led/Core/Src/main.c
+++ 02_blinking_led/Core/Src/main.c
@@ -1,17 +1,76 @@
 #include "stdint.h"
 //#include "stm32f103xb.h"
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_conf.h"
 
+void setUpHSE();
 void setUpLed();
 void setLight();
 void resetLight();
 
+uint32_t flag = 0;
 int main(void)
 {
+    setUpHSE();
     setUpLed();
     setLight();
     while (1)
-  {
-  }
+    {
+    }
+}
+
+void setUpHSE()
+{
+    __IO uint32_t cnt = 0;
+    __IO uint32_t HSEStatus = 0;
+
+    RCC->CR = ((uint32_t)RCC_CR_HSEON);
+
+    do
+    {
+        HSEStatus = RCC->CR & RCC_CR_HSERDY;
+        ++cnt;
+    } while (HSEStatus == 0 && cnt != HSE_STARTUP_TIMEOUT);
+
+    if ((RCC->CR && RCC_CR_HSERDY) != RESET)
+    {
+        HSEStatus = 0x1;
+    }
+    else
+    {
+        HSEStatus = 0x0;
+    }
+
+    if (HSEStatus == 0x1)
+    {
+        FLASH->ACR |= FLASH_ACR_PRFTBE;
+
+        FLASH->ACR &= (uint32_t)((uint32_t) ~FLASH_ACR_LATENCY);
+        FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+
+
+        // AHB prescaler
+        RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+
+        // Как я понял это множитель на шину APB. Светодиод там.
+        // Настраивать другие множители не нужно
+        // APB2 prescaler
+        RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+
+        // Как я это понимаю --> сбрасываем настройки pll модулей
+        RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
+        RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC | RCC_CFGR_PLLMULL9);
+
+        RCC->CR |= RCC_CR_PLLON;
+
+
+        while ((RCC->CR & RCC_CR_PLLRDY) == 0);
+
+        RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+        RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+
+        while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (0x08));
+    }
 }
 
 void setUpLed()
